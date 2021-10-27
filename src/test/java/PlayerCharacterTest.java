77ba041840276610a55c5cf573b93de91d753f48
Erik Gustafsson <erikgust2@gmail.com>
2021-10-27 17:13:56 +0200

    Tog bort mockito från PlayerCharacterTest

--- src/test/java/PlayerCharacterTest.java
+++ src/test/java/PlayerCharacterTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class PlayerCharacterTest {
 
